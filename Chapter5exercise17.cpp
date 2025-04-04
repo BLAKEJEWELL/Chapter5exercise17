5e87cb281eead9225e47f32f93b5db6c12035939
BLAKEJEWELL <bjewell1833@gmail.com>
2025-04-04 08:19:39 -0400

    added options for sub, mult, div, and an exit

--- Chapter5exercise17.cpp
+++ Chapter5exercise17.cpp
@@ -13,22 +13,40 @@ should then tell the user if they are correct or not.
 #include <cstdlib>
 
 int random();
-void addition(int, int);
-char choice_();
+void addition();
+void subraction();
+void multiplication();
+void division();
 void checking_answer(int, int);
+int menu();
 
 using namespace std;
 
 int main()
 {
-	char choice;
-	do
+	int choice = menu();
+	do 
 	{
-		int number1 = random();
-		int number2 = random();
-		addition(number1, number2);
-		choice = choice_();
-	} while (choice == 'y' || choice == 'Y');
+		
+		switch (choice)
+		{
+		case 1:
+			addition();
+			break;
+		case 2:
+			subraction();
+			break;
+		case 3:
+			multiplication();
+			break;
+		case 4:
+			division();
+			break;
+			
+		}
+		int choice = menu();
+	} while (choice <1 && choice > 4);
+	return 0;
 
 }
 int random() //This function generates random numbers
@@ -39,8 +57,10 @@ int random() //This function generates random numbers
 	int number1 = number(math);                      //defining random intergers
 	return number1;
 }
-void addition(int num1, int num2) //This function takes in numbers, adds them together, and displays this info for the user
+void addition() //This function takes in numbers, adds them together, and displays this info for the user
 {
+	int num1 = random();
+	int num2 = random();
 
 	cout << "Add these 2 numbers together. Then type in your answer and hit the Enter key" << endl;
 	cout << "\n";
@@ -52,26 +72,59 @@ void addition(int num1, int num2) //This function takes in numbers, adds them to
 	int useranswer;
 	cin >> useranswer;
 	
-	checking_answer(useranswer, answer);
+	checking_answer(answer, useranswer);
 
 }
-char choice_() //This function asks the user to exit or continue the program
+void subraction()
 {
-	char question;
-	cout << "Would you like to do another equation? (Y/N)" << endl;
-	cin >> question;
-	while (question != 'Y' && question != 'y' && question != 'N' && question != 'n')
-	{
-		cout << "Invalid choice. Please enter either (Y), for yes, or (N), for no." << endl;
-		cin >> question;
-	}
-	if (question == 'N' || question == 'n')
-	{
-		cout << "Goodbye" << endl;
-		exit(0);
-	}
-	return question;
+	int num1 = random();
+	int num2 = random();
 
+	cout << "Subtract these two numbers. Then type in your answer and hit the Enter key" << endl;
+	cout << "\n";
+
+	cout << setw(10) << num1 << endl;
+	cout << setw(7) << "-" << num2 << endl;
+
+	int answer = num1 - num2;
+	int useranswer;
+	cin >> useranswer;
+
+	checking_answer(answer, useranswer);
+}
+void multiplication()
+{
+	int num1 = random();
+	int num2 = random();
+
+	cout << "Multiply these 2 numbers together. Then type in your answer and hit the Enter key" << endl;
+	cout << "\n";
+
+	cout << setw(10) << num1 << endl;
+	cout << setw(7) << "x" << num2 << endl;
+
+	int answer = num1 * num2;
+	int useranswer;
+	cin >> useranswer;
+
+	checking_answer(answer, useranswer);
+}
+void division()
+{
+	int num1 = random();
+	int num2 = random();
+
+	cout << "Divide these two numbers. Then type in your answer and hit the Enter key" << endl;
+	cout << "\n";
+
+	cout << setw(10) << num1 << endl;
+	cout << setw(7) << "÷" << num2 << endl;
+
+	int answer = num1 / num2;
+	int useranswer;
+	cin >> useranswer;
+
+	checking_answer(answer, useranswer);
 }
 void checking_answer(int answer, int useranswer) //This function lets the user know if they are right or wrong
 {
@@ -83,6 +136,29 @@ void checking_answer(int answer, int useranswer) //This function lets the user k
 	{
 		cout << "Incorrect. Answer is "<<answer << endl;
 	}
+}
+int menu()
+{
+	int selection;
+	cout << "PLEASE SELECT ONE OF THE FOLLOWING MENU OPTIONS (1-5)" << endl;
+	cout << "**********************" << endl;
+	cout << "(1) Addition" << endl;
+	cout << "(2) Subtraction" << endl;
+	cout << "(3) Multiplication" << endl;
+	cout << "(4) Division" << endl;
+	cout << "(5) Exit " << endl;
+	cin >> selection;
+	while (selection < 1 || selection > 5)
+	{
+		cout << "Invalid selection. Please enter one of the options listed above (1-5) and press ENTER." << endl;
+		cin >> selection;
+
+	}
+	if (selection == 5)
+	{
+		exit(0);
+	}
+	return selection;
 }
 
 
